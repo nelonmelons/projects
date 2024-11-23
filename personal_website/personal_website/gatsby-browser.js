12f9adb75f935bb0dba445a03f20092733d29ef5
nelonmelons <nelsonsiu508@gmail.com>
2024-11-22 21:16:17 -0500

    added bridge scripts

--- personal_website/personal_website/gatsby-browser.js
+++
@@ -1,9 +0,0 @@
-/**
- * Implement Gatsby's Browser APIs in this file.
- *
- * See: https://www.gatsbyjs.com/docs/reference/config-files/gatsby-browser/
- */
-
-// You can delete this file if you're not using it
-// gatsby-browser.js
-import './src/styles/global.css';
--- personal_website/personal_website/src/blog/my-first-post.md
+++
@@ -1,6 +0,0 @@
----
-title: 'My First Blog Post'
-date: '2024-10-01'
----
-
-This is the content of my first blog post.
